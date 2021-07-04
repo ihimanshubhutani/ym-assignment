cca9be62d7139cdc9df27616f54a0e1d2a794085
Himanshu Bhutani <bhutanihimanshu98@gmail.com>
2021-07-04 22:20:34 +0530

    feat(controller): Add url controllers

---
+++ src/controller/url.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express"
+import {
+  getShortendCodeUrl,
+  generateShortenedCode,
+} from "../helper/urlFunctions"
+
+/**
+ * Redirects User to Orignal URL
+ * @param req : Express Request Object
+ * @param res : Express Response Object
+ * @returns   : Response, redirects user to URL
+ */
+
+export const getOriginalUrl = async (
+  req: Request,
+  res: Response,
+): Promise<void | Response> => {
+  const { shortnedUrlCode } = req.params
+  const orignalUrl = await getShortendCodeUrl(shortnedUrlCode)
+  if (!orignalUrl) {
+    return res.status(404).send({ msg: "Invalid Code" })
+  }
+  return res.redirect(orignalUrl)
+}
+
+/**
+ * Converts Orignal URL to Shortened URL
+ * @param req : Express Request Object
+ * @param res : Express Response Object
+ * @returns   : Response {code}, Shortened URL code
+ */
+
+export const getShortenedUrl = async (
+  req: Request,
+  res: Response,
+): Promise<Response> => {
+  const { url } = req.body
+
+  const shortnedUrlCode = await generateShortenedCode(url)
+  return res.json({ code: shortnedUrlCode })
+}

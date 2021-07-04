39f7c52b643b5b1ef8280464b2b7e0d45f8db448
Himanshu Bhutani <bhutanihimanshu98@gmail.com>
2021-07-04 22:24:00 +0530

    refacator(config): Update env config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
-export const PORT = process.env.PORT || 3000
+import * as dotenv from "dotenv"
+dotenv.config()
 
+export const PORT = process.env.PORT || 3000
 export const REDIS_HOST = process.env.REDIS_HOST || "localhost"
-export const REDIS_PORT = process.env.REDIS_PORT || 6379
+export const REDIS_PORT = process.env.REDIS_PORT || 6378
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD || undefined

ec37aaebefb8bd0cbbf6e965052f389b3af90894
Himanshu Bhutani <bhutanihimanshu98@gmail.com>
2021-07-05 06:04:16 +0530

    Output Cluster name and ECR repo url

---
+++ terraform/environments/development/outputs.tf
@@ -0,0 +1,7 @@
+output "cluster_name" {
+  value = module.cluster.cluster_name
+}
+
+output "ym_app_ecr_url" {
+  value = module.ecr.ym_app_ecr_url
+}
---
+++ terraform/modules/ecr/outputs.tf
@@ -0,0 +1,3 @@
+output "ym_app_ecr_url" {
+  value = aws_ecr_repository.ym_app_ecr.repository_url
+}
